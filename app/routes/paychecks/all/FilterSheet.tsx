d5763f18b3f1b2cb51d2271b55b0b91b12eaf779
Luis <luchomame@gmail.com>
2025-06-14 20:04:05 -0400

    finished extracting FAB

--- app/routes/paychecks/all/FilterSheet.tsx
+++ app/routes/paychecks/all/FilterSheet.tsx
@@ -10,6 +10,7 @@ import {
 } from "~/components/ui/sheet";
 import { SlidersHorizontal } from "lucide-react";
 import { Label } from "~/components/ui/label";
+import FAB from "~/components/FAB";
 
 type props = {
   filterSheetOpen: boolean;
@@ -29,13 +30,10 @@ export default function FilterSheet({
   return (
     <Sheet open={filterSheetOpen} onOpenChange={setFilterSheetOpen}>
       <SheetTrigger asChild>
-        <Button
+        <FAB
           onClick={() => setFilterSheetOpen(true)}
-          className="fixed bottom-6 right-6 z-50 p-0 rounded-full bg-primary text-primary-foreground shadow-lg flex items-center justify-center"
-          size="fab"
-        >
-          <SlidersHorizontal className="w-5 h-5" strokeWidth={2.5} />
-        </Button>
+          Icon={SlidersHorizontal}
+        />
       </SheetTrigger>
 
       <SheetContent side="bottom" className="p-6 max-w-md mx-auto">
